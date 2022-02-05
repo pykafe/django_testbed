2f86fe7373c8cd1a1e64fddcb19dc0b952b4bd15
PeteCoward <peter@catalpa.io>
2022-02-05 11:08:15 +0900

    Hook up wc implementation

--- testbed/python_function_pages/libraries/word_count.py
+++ testbed/python_function_pages/libraries/word_count.py
@@ -1,2 +1,15 @@
 def wc(text):
-    return "Your result"
+    lines = text.splitlines()
+        
+    lines_count = 0
+    words_count = 0
+    characters_count = 0
+    
+    for line_character in lines:
+        words = line_character.split()
+        
+        lines_count += 1
+        words_count += len(words)
+        characters_count += len(line_character)
+        
+    return f"{lines_count}, {words_count}, {characters_count}"
