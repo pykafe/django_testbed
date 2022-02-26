8c355d72ffe93e5ee404d4cd1a2e9567cf9b12ce
Anders Hofstee <a.hofstee@catalpainternational.org>
2022-02-26 11:30:32 +0900

    This version of word count uses HTMX.

--- testbed/python_function_pages/urls.py
+++ testbed/python_function_pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, WordCountView
+from .views import IndexView, WordCountView, count_words
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('word_count', WordCountView.as_view(), name='word_count'),
+    path('count_words', count_words, name='count_words'),
 ]
--- testbed/python_function_pages/views.py
+++ testbed/python_function_pages/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 
 from .libraries.word_count import word_count
@@ -10,19 +11,10 @@ class IndexView(TemplateView):
 class WordCountView(TemplateView):
     template_name = "python_function_pages/word_count.html"
 
-    def post(self, request, *args, **kwargs):
-        # use request.POST["text"] here in our functions which count words
-        # add the result to the request
 
-        if "text" in request.POST:
-            self.word_count_result = word_count(request.POST["text"])
-        elif "upload_file" in request.FILES:
-            self.word_count_result_file = word_count(request.FILES["upload_file"].read())
-
-        return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['result'] = getattr(self, 'word_count_result', "Submit the form to count words")
-        context['file_result'] = getattr(self, 'word_count_result_file', "Submit the form to count words")
-        return context
+def count_words(request):
+    if "text" in request.POST:
+            word_count_result = word_count(request.POST["text"])
+    elif "upload_file" in request.FILES:
+            word_count_result = word_count(request.FILES["upload_file"].read())
+    return HttpResponse(word_count_result)
