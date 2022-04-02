b440de30378df0d4cd1ae34c08d41705b3edaf58
PeteCoward <peter@catalpa.io>
2022-04-02 11:58:40 +0900

    HTMX Approval status toggle in list

--- testbed/projects/urls.py
+++ testbed/projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProjectListView, ProjectApproveView
+from .views import ProjectListView, ProjectApproveView, ProjectRejectView
 
 urlpatterns = [
     path('project_list', ProjectListView.as_view(), name='project_list'),
-    path('approve_project', ProjectApproveView.as_view(), name='approve_project'),
+    path('approve_project/<int:pk>/', ProjectApproveView.as_view(), name='approve_project'),
+    path('reject_project/<int:pk>/', ProjectRejectView.as_view(), name='reject_project'),
 ]
--- testbed/projects/views.py
+++ testbed/projects/views.py
@@ -12,10 +12,29 @@ class ProjectListView(TemplateView):
         return context
 
 
-class ProjectApproveView(TemplateView):
-    template_name = "projects/project_approval_fragment.html"
+class ProjectApprovalStatusBaseView(TemplateView):
+    """Base class for views that update a project apporval status"""
+    template_name = "projects/partials/project_approval.html"
+    new_approval_status = Project.APPROVAL_APPROVED
+    
+    def post(self, request, pk):
+        # this might be better, as only one sql statement
+        # Project.objects.filter(pk=pk).update(approval_status=Project.APPROVAL_APPROVED)
+        project = Project.objects.get(pk=pk)
+        project.approval_status = self.new_approval_status
+        project.save()
+        self.project = project
+        return super().get(request)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['projects'] = Project.objects.all()
+        context['project'] = self.project
         return context
+
+
+class ProjectRejectView(ProjectApprovalStatusBaseView):
+    new_approval_status = Project.APPROVAL_REJECTED
+
+
+class ProjectApproveView(ProjectApprovalStatusBaseView):
+    new_approval_status = Project.APPROVAL_APPROVED
