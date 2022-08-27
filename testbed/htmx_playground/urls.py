d83966d7a9e6e71fa08f2d150e60b0f65afedd1b
Anders Hofstee <a.hofstee@catalpainternational.org>
2022-08-27 12:48:55 +0900

    adds example of htmx using a partial template and targetting a div

--- testbed/htmx_playground/urls.py
+++ testbed/htmx_playground/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, RandomNumberView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('random', RandomNumberView.as_view()),
 ]
--- testbed/htmx_playground/views.py
+++ testbed/htmx_playground/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
-# Create your views here.
+import random
+
+
 class IndexView(TemplateView):
     template_name = "htmx_playground.html"
 
 
+class RandomNumberView(TemplateView):
+    template_name = "partial__random_number.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['random_number'] = str(random.random())[5]
+        return context
